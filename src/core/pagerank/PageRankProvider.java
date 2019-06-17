175087813349ae6f57f85710137924089786380c
MasudRahman <masudcseku@gmail.com>
2019-06-17 14:17:12 -0600

    PageRank calculator updated

--- src/core/pagerank/PageRankProvider.java
+++ src/core/pagerank/PageRankProvider.java
@@ -1,4 +1,4 @@
-package core;
+package core.pagerank;
 
 import java.util.HashMap;
 import java.util.Set;
--- src/core/pagerank/StackGraph.java
+++ src/core/pagerank/StackGraph.java
@@ -1,4 +1,4 @@
-package core;
+package core.pagerank;
 
 import java.awt.Color;
 import java.awt.Dimension;
